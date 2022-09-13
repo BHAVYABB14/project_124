e12a869efd74a552d155784b1550bf447a1a3fc2
BHAVYABB14 <90505667+BHAVYABB14@users.noreply.github.com>
2022-09-13 18:13:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
-function setup(){
-    video = createCapture(VIDEO);
-    video.size(550,500);
-    canvas = createCanvas(550,500);
-    canvas.position(560,150);
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose', gotPoses);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-function modelLoaded(){
-    console.log('poseNet Is Initialized !')
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
-function draw(){
-    background('#0000ff');
-    document.getElementById("square_side").innerhtml = "width And Height of a Square will be -" + difference +"px";
-    fill('#9900ff');
-    stroke('#9900ff');
-    square(noseX,noseY,difference);
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
-        noseX = results[0].pose.nose.X;
-        noseY = results[0].pose.nose.Y;
-        console.log("noseX = " + noseX +"noseY = "+noseY);
-        leftWristX = results[0].pose.leftWrist.x;
-        rightWristX = results[0].pose.rightWrist.x;
-        difference = floor(leftWristX - rightWristX);
-        console.log("leftWristX = " + leftWristX +"rightWristX ="+ rightWristX + "difference =" +difference);
-    }
+
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "FONT SIZE OF THE TEXT WILL BE = " + difference +"px";
+  fill('#F90093');
+  textSize(difference);
+  text('PeTer',50,400);
 }
